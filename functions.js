7f731f3656ea65358247082c495c667f5abe0bbd
samucorea <samuel.alejandro100@hotmail.com>
2021-08-06 20:46:19 -0400

    contact book finished

--- functions.js
+++ functions.js
@@ -6,33 +6,63 @@ function insertContact(db, contact) {
 
 }
 
-function createContactNode(contact) {
+function deleteContact(db, contact) {
+    db.removeItem(contact.key)
+    location.replace('/')
+}
+
+function setTemporaryInputValues(db, contact) {
+
+    db.setItem('tempValues', JSON.stringify(contact))
+
+}
+
+
+function editContact(db, contact) {
+    deleteContact(db, contact)
+    setTemporaryInputValues(db, contact)
+
+}
+function createContactNode(db, contact, parentNode) {
     const contactDiv = document.createElement('div')
     const nameh3 = document.createElement('h3')
     const phoneNumberDiv = document.createElement('div')
     const addressDiv = document.createElement('div')
     const deleteSpan = document.createElement('span')
+    const editSpan = document.createElement('span')
+
+
 
     contactDiv.classList.add('contact')
     deleteSpan.classList.add('material-icons', 'icon')
+    editSpan.classList.add('material-icons')
+
 
     nameh3.textContent = contact.name
     phoneNumberDiv.textContent = contact.phoneNumber
     addressDiv.textContent = contact.address
-    deleteSpan.innerHTML = 'delete'
+    deleteSpan.textContent = 'delete'
+    editSpan.textContent = 'edit'
 
-    contactDiv.append(nameh3, phoneNumberDiv, addressDiv, deleteSpan)
 
-    return contactDiv
+    editSpan.style.cursor = 'pointer'
+
+
+    deleteSpan.onclick = () => deleteContact(db, contact)
+    editSpan.onclick = () => editContact(db, contact)
+
+    contactDiv.append(nameh3, phoneNumberDiv, addressDiv, deleteSpan, editSpan)
+
+    parentNode.appendChild(contactDiv)
 }
+
 
 function loadContacts(parentNode, db) {
     const contactKeys = Object.keys(db)
 
     for (let key of contactKeys) {
         const contact = JSON.parse(db.getItem(key))
-        const contactElement = createContactNode(contact)
-        parentNode.appendChild(contactElement)
+        createContactNode(db, contact, parentNode)
+
     }
 }
-
--- main.js
+++ main.js
@@ -6,8 +6,32 @@ const insertBtn = document.querySelector('#insert-btn')
 const contactList = document.querySelector('.contact-list')
 const db = window.localStorage
 
+const tempValues = JSON.parse(db.getItem('tempValues'))
 
+if (tempValues) {
+    contactName.value = tempValues.name || ''
+    phoneNumber.value = tempValues.phoneNumber || ''
+    address.value = tempValues.address || ''
+    db.removeItem('tempValues')
+}
+
+
+phoneNumber.addEventListener('keydown', e => {
+    if (isNaN(e.target.value)) {
+        e.preventDefault()
+    }
+    else {
+        console.log('yes')
+    }
+})
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        insertBtn.click()
+    }
+})
 insertBtn.onclick = () => {
+    if (contactName.value === '' || phoneNumber.value === '' || address.value === '') return;
     const contact = {
         key: Math.random(1, 100),
         name: contactName.value,
