85a58c609755cff644fe2c6a92c25b3e68420872
samucorea <samuel.alejandro100@hotmail.com>
2021-08-07 00:02:02 -0400

    documenting code.

--- functions.js
+++ functions.js
@@ -1,14 +1,14 @@
 function insertContact(db, contact) {
 
-    db.setItem(contact.key, JSON.stringify(contact))
+    db.setItem(contact.key, JSON.stringify(contact))//we save the contact on db with a key since the localStorage works as a dictionary.
 
-    location.replace('/')
+    location.replace('/')//This is a shortcut for reseting input values. we force a redirect.
 
 }
 
 function deleteContact(db, contact) {
-    db.removeItem(contact.key)
-    location.replace('/')
+    db.removeItem(contact.key)//remove the item from the localStorage using the key.
+    location.replace('/')//Again, a shortcut for displaying the data without the deleted contact.
 }
 
 function setTemporaryInputValues(db, contact) {
@@ -19,11 +19,13 @@ function setTemporaryInputValues(db, contact) {
 
 
 function editContact(db, contact) {
-    deleteContact(db, contact)
-    setTemporaryInputValues(db, contact)
+    deleteContact(db, contact)// We delete the contact from the db.
+    setTemporaryInputValues(db, contact)// Then we set the temporary values for the inputs.
 
 }
 function createContactNode(db, contact, parentNode) {
+
+    //create html elements.
     const contactDiv = document.createElement('div')
     const nameh3 = document.createElement('h3')
     const phoneNumberDiv = document.createElement('div')
@@ -32,34 +34,36 @@ function createContactNode(db, contact, parentNode) {
     const editSpan = document.createElement('span')
 
 
-
+    //add the different classes to the html elements.
     contactDiv.classList.add('contact')
     deleteSpan.classList.add('material-icons', 'icon')
     editSpan.classList.add('material-icons')
 
-
+    //add the text content for each element, that will contain the contact information, and two icons to delete and edit a specific contact.
     nameh3.textContent = contact.name
     phoneNumberDiv.textContent = contact.phoneNumber
     addressDiv.textContent = contact.address
     deleteSpan.textContent = 'delete'
     editSpan.textContent = 'edit'
 
-
+    //some styles to the edit icon.
     editSpan.style.cursor = 'pointer'
 
-
+    //add events to the delete and edit button, which will execute their respective function.
     deleteSpan.onclick = () => deleteContact(db, contact)
     editSpan.onclick = () => editContact(db, contact)
-
+    //append everything to parent div.
     contactDiv.append(nameh3, phoneNumberDiv, addressDiv, deleteSpan, editSpan)
-
+    //append parent div to the parentNode of the document.
     parentNode.appendChild(contactDiv)
 }
 
 
 function loadContacts(parentNode, db) {
+    //get all contact keys of localStorage.
     const contactKeys = Object.keys(db)
-
+    //for each key, get the contact on the db, and create a contact node based on that information.
+    //The data of the contact must be parsed.
     for (let key of contactKeys) {
         const contact = JSON.parse(db.getItem(key))
         createContactNode(db, contact, parentNode)
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//Queries to get input values and insert button.
 const contactName = document.querySelector('#name')
 const phoneNumber = document.querySelector('#phone-number')
 const address = document.querySelector('#address')
@@ -6,41 +8,48 @@ const insertBtn = document.querySelector('#insert-btn')
 const contactList = document.querySelector('.contact-list')
 const db = window.localStorage
 
+
+//Get temporary values of localStorage in case the user decides to edit a contact
 const tempValues = JSON.parse(db.getItem('tempValues'))
 
+//If tempValues are not null, that means that the user wanted to edit some contact. In that case, add the values of that contact
+//to the input elements, so the user can edit them and re-insert them.
 if (tempValues) {
-    contactName.value = tempValues.name || ''
-    phoneNumber.value = tempValues.phoneNumber || ''
-    address.value = tempValues.address || ''
-    db.removeItem('tempValues')
+    contactName.value = tempValues.name
+    phoneNumber.value = tempValues.phoneNumber
+    address.value = tempValues.address
+    db.removeItem('tempValues')//remove the tmp values.
 }
 
+// //number validation for phone Number input. isNaN is a function to establish if a value is a Not A Number.
+// phoneNumber.addEventListener('keydown', e => {
+//     if (isNaN(e.target.value)) {
+//         e.target.value.splice(0, e.target.value.length - 2)
+//     }
 
-phoneNumber.addEventListener('keydown', e => {
-    if (isNaN(e.target.value)) {
-        e.preventDefault()
-    }
-    else {
-        console.log('yes')
-    }
-})
+// })
 
+// Shortcut for inserting a contact with Enter key. To avoid repeating logic, simply click the insert button.
 document.addEventListener('keydown', e => {
     if (e.key === 'Enter') {
         insertBtn.click()
     }
 })
+
+// Event  clicking insertBtn
 insertBtn.onclick = () => {
+    //If  any values are empty, do nothing.
     if (contactName.value === '' || phoneNumber.value === '' || address.value === '') return;
+    //We'll create a contact object with all the information to simplify the data processing.
     const contact = {
-        key: Math.random(1, 100),
+        key: Math.random(1, 100),//The key will be useful when entering the information on localStorage.
         name: contactName.value,
         phoneNumber: phoneNumber.value,
         address: address.value,
     }
 
 
-    insertContact(db, contact)
+    insertContact(db, contact)//We insert the contact to the local storage or db.
 }
 
-loadContacts(contactList, db)
+loadContacts(contactList, db)//Load all contacts of local storage.
