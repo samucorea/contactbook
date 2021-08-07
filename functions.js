8872c8682c033842b10111321f9bb2adb8f87c3a
samucorea <samuel.alejandro100@hotmail.com>
2021-08-07 00:24:35 -0400

    change redirect

--- functions.js
+++ functions.js
@@ -2,13 +2,13 @@ function insertContact(db, contact) {
 
     db.setItem(contact.key, JSON.stringify(contact))//we save the contact on db with a key since the localStorage works as a dictionary.
 
-    location.replace('/')//This is a shortcut for reseting input values. we force a redirect.
+    location.href = '/'//This is a shortcut for reseting input values. we force a redirect.
 
 }
 
 function deleteContact(db, contact) {
     db.removeItem(contact.key)//remove the item from the localStorage using the key.
-    location.replace('/')//Again, a shortcut for displaying the data without the deleted contact.
+    location.href = '/'//Again, a shortcut for displaying the data without the deleted contact.
 }
 
 function setTemporaryInputValues(db, contact) {
