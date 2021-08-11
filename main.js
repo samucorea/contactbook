a778fe8e3c1d20e0e447bbb706b10fc9b2a99f97
samucorea <samuel.alejandro100@hotmail.com>
2021-08-11 12:58:28 -0400

    edit functionality begin

--- main.js
+++ main.js
@@ -3,6 +3,13 @@
 const contactName = document.querySelector('#name')
 const phoneNumber = document.querySelector('#phone-number')
 const address = document.querySelector('#address')
+
+const overlay = document.querySelector('.overlay')
+const editContactName = document.querySelector('#edit-name')
+const editPhoneNumber = document.querySelector('#edit-phone-number')
+const editAddress = document.querySelector('#edit-address')
+const editBtn = document.querySelector('#edit-btn')
+
 const insertBtn = document.querySelector('#insert-btn')
 const errorMessage = document.querySelector('.error-message')
 
@@ -16,18 +23,40 @@ const tempValues = JSON.parse(db.getItem('tempValues'))
 //If tempValues are not null, that means that the user wanted to edit some contact. In that case, add the values of that contact
 //to the input elements, so the user can edit them and re-insert them.
 if (tempValues) {
-    contactName.value = tempValues.name
-    phoneNumber.value = tempValues.phoneNumber
-    address.value = tempValues.address
-    db.removeItem('tempValues')//remove the tmp values.
+    overlay.style.display = 'initial'
+    editContactName.value = tempValues.name
+    editPhoneNumber.value = tempValues.phoneNumber
+    editAddress.value = tempValues.address
+
+    editBtn.onclick = () => {
+        const editedContact = {
+            key: tempValues.key,
+            name: editContactName.value,
+            phoneNumber: editPhoneNumber.value,
+            address: editAddress.value
+        }
+
+        insertContact(db, editedContact)
+        overlay.style.display = 'none'
+        db.removeItem('tempValues')
+    }
+
 }
 
 
 // Shortcut for inserting a contact with Enter key. To avoid repeating logic, simply click the insert button.
 document.addEventListener('keydown', e => {
     if (e.key === 'Enter') {
-        insertBtn.click()
+        if (overlay.style.display === 'initial') {
+            editBtn.click()
+
+        }
+        else {
+            insertBtn.click()
+        }
+
     }
+
 })
 
 // Event  clicking insertBtn
