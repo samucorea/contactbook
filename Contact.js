29d93c22b31a20ad4e213761a4c18a7e2020be22
samucorea <samuel.alejandro100@hotmail.com>
2021-08-12 17:04:19 -0400

    refactor. Contact class created and use strict implemented.

---
+++ Contact.js
@@ -0,0 +1,11 @@
+'use strict';
+class Contact {
+
+    constructor(key, name, phoneNumber, address) {
+        this.key = key;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+}
