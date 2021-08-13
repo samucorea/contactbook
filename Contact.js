7c75a06aac61755b8bff5a25d89846eb1a5e44fe
samucorea <samuel.alejandro100@hotmail.com>
2021-08-13 14:56:58 -0400

    refactor Contact class

--- Contact.js
+++ Contact.js
@@ -1,8 +1,8 @@
 'use strict';
 class Contact {
 
-    constructor(key, name, phoneNumber, address) {
-        this.key = key;
+    constructor(name, phoneNumber, address, key = null) {
+        this.key = key || Math.random(1, 100);
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.address = address;
